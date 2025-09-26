e3de79bf182402710e221a4420b8eaf8765de6aa
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-09-26 21:25:32 +0530

    defining functions to save data and adding hours

--- code.py
+++ code.py
@@ -9,4 +9,18 @@ def load_data():
         with open(filename, "r") as file:
             return json.load(file)
         return{}
-    
+
+#saving data:
+def save_data(data):
+    with open(FILENAME, "w") as file:
+        json.dump(data, file, indent=4)
+
+#adding study hours;
+def add_hours(subjects, hours):
+    data = load_data
+    if subject in data:
+        data[subject] += hours
+    else: 
+        data[subject] = hours
+    save_data(data)
+    print(f"Added {hours} hours to {subjects}. Total = {data[subject]} hrs")
