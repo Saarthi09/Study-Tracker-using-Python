3143531bc0180ab8c5c95c53375b361f42014d57
Saarthi09 <saarthibasantgupta31@gmail.com>
2025-09-29 18:32:02 +0530

    Final code and corrections in previous code

--- code.py
+++ code.py
@@ -8,16 +8,16 @@ def load_data():
     if os.path.exists(filename):
         with open(filename, "r") as file:
             return json.load(file)
-        return{}
+    return{}
 
 #saving data:
 def save_data(data):
-    with open(FILENAME, "w") as file:
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 #adding study hours;
 def add_hours(subjects, hours):
-    data = load_data
+    data = load_data()
     if subject in data:
         data[subject] += hours
     else: 
@@ -35,4 +35,28 @@ def view_stats():
     for subject, hours in data.items():
         print(f"- {subject}: {hours} hrs")
         total += hours
-    print{f"\n total study time: {total}hrs"}
+    print(f"\n total study time: {total}hrs")
+
+def main():
+    while True:
+        print("\n1. Add study hours")
+        print("2. View Stats")
+        print("3. Exit")
+        choice = input("Choose: ")
+
+        if choice == "1":
+            subject = input("Enter subject name: ").strip()
+            try:
+                hours = float(input("Enter hours studied:"))
+                add_hours(subject, hours)
+            except ValueError:
+                print("Please enter a valid number")
+        elif choice == "2":
+            view_stats()
+        elif choice =="3":
+            print("Goodbye")
+        else:
+            print("Invalid choice")
+        
+if __name__ == "__main__":
+    main()
